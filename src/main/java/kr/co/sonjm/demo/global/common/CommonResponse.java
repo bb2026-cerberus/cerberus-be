3d954b083ba5e29f56c9e96b7a3d69d1d9661653
SonJM <blisle@naver.com>
2025-11-24 17:58:14 +0900

    chore: 그룹 명 변경

--- src/main/java/kr/co/sonjm/demo/global/common/CommonResponse.java
+++
@@ -1,8 +0,0 @@
-package kr.co.sonjm.demo.global.common;
-
-import lombok.Builder;
-import org.springframework.http.HttpStatus;
-
-@Builder
-public record CommonResponse(HttpStatus status, String message, Object data) {
-}
