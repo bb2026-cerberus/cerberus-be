4fcd759cb777ff599d019f1c0740791eb95ff4ee
SonJM <blisle@naver.com>
2026-02-09 22:29:06 +0900

    fix: 배포 Swagger CORS 이슈 수정

--- src/main/java/kr/co/cerberus/global/config/OpenApiConfig.java
+++ src/main/java/kr/co/cerberus/global/config/OpenApiConfig.java
@@ -1,21 +1,21 @@
-//package kr.co.cerberus.global.config;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.servers.Server;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.List;
-//
-//@Configuration
-//public class OpenApiConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        Server server = new Server();
-//        server.setUrl("https://api.seolberus.co.kr");
-//        server.setDescription("Production Server (HTTPS)");
-//
-//        return new OpenAPI().servers(List.of(server));
-//    }
-//}
+package kr.co.cerberus.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://api.seolberus.co.kr");
+        server.setDescription("Production Server (HTTPS)");
+
+        return new OpenAPI().servers(List.of(server));
+    }
+}
