8cec622b0ffc59ec2d8c9f4aaae76e9f7203acdf
JangEunJi <jej132456@gmail.com>
2026-02-11 04:18:18 +0900

    feat: 스케줄러 어노테이션 추가

--- src/main/java/kr/co/cerberus/CerberusApplication.java
+++ src/main/java/kr/co/cerberus/CerberusApplication.java
@@ -3,7 +3,9 @@ package kr.co.cerberus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableAsync
 @SpringBootApplication
 public class CerberusApplication {
